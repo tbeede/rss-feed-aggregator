ec4e0d3722142e5724064a63fd41af5e9f57db77
tbeede <trrbeede@gmail.com>
2018-01-24 21:15:00 -0500

    get something started

---
+++ src/main/java/com/truenews/TrueNewsApplication.java
@@ -0,0 +1,12 @@
+package com.truenews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueNewsApplication.class, args);
+	}
+}
