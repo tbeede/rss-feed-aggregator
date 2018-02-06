945997a0e5a24dacd2ade7ac0bcf6ca11f6590d9
tbeede <trrbeede@gmail.com>
2018-02-05 20:55:18 -0500

    Refactor

--- src/main/java/com/truenews/RSSController.java
+++ src/main/java/com/truenews/RSSController.java
@@ -4,16 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import static com.truenews.RSSParser.readRSSFeed;
-
 @Controller
-public class UrlController {
+public class RSSController {
     @GetMapping(value = "/")
     public String rssForm(Model model) {
         model.addAttribute("rssParser", new RSSParser());
-        String rssTest = "http://rss.cnn.com/rss/edition.rss";
-        readRSSFeed(rssTest);
         return "index";
     }
-
 }
