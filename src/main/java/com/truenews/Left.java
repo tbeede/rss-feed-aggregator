989e02b737970153288ce3fcce9178e04bb74900
tbeede <trrbeede@gmail.com>
2018-02-18 13:00:13 -0500

    Add RSS feed from WaPo

--- src/main/java/com/truenews/Left.java
+++ src/main/java/com/truenews/Left.java
@@ -1,18 +1,9 @@
 package com.truenews;
 
-import java.util.ArrayList;
-
 public class Left {
     private String cnn = "http://rss.cnn.com/rss/cnn_allpolitics.rss";
     private String nyt = "http://rss.nytimes.com/services/xml/rss/nyt/Politics.xml";
-
-    private final ArrayList<String> leftList = new ArrayList<>();
-
-    public ArrayList<String> getLeftList() {
-        leftList.add(getCnn());
-        leftList.add(getNyt());
-        return leftList;
-    }
+    private String waPo = "http://feeds.washingtonpost.com/rss/politics";
 
     public String getCnn() {
         return cnn;
@@ -29,4 +20,13 @@ public class Left {
     public void setNyt(String nyt) {
         this.nyt = nyt;
     }
+
+    public String getWaPo() {
+        return waPo;
+    }
+
+    public void setWaPo(String waPo) {
+        this.waPo = waPo;
+    }
+
 }
