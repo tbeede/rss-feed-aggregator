fa4d4e6405db21cbc6a84ede859e1543b9de847c
tbeede <trrbeede@gmail.com>
2018-02-18 13:01:04 -0500

    Use ROME API for prettier, cleaner parsing

--- src/main/java/com/truenews/RSSParser.java
+++ src/main/java/com/truenews/RSSParser.java
@@ -1,60 +1,27 @@
 package com.truenews;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.*;
 import java.io.IOException;
+import java.net.URL;
 
 public class RSSParser {
-    // For testing
-    public static void main(String[] args) {
-        readRSSFeed();
-    }
-    public static String readRSSFeed() {
-        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder;
-        Document doc = null;
-        Left left = new Left();
-        try {
-            builder = domFactory.newDocumentBuilder();
-//            doc = builder.parse("http://rss.nytimes.com/services/xml/rss/nyt/Politics.xml");
-            doc = builder.parse(left.getCnn());
-        }
-        catch (SAXException e) {
-            e.printStackTrace();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        catch (ParserConfigurationException e) {
-            e.printStackTrace();
+    public static String readRssFeed(String feedUrl) throws IOException, FeedException {
+        final URL feedSource = new URL(feedUrl);
+        final SyndFeedInput input = new SyndFeedInput();
+        final SyndFeed feed = input.build(new XmlReader(feedSource));
+        String parsedFeed = "";
+        for(Object o: feed.getEntries()) {
+            SyndEntry entry = (SyndEntry) o;
+            parsedFeed += "<p>" + "<h4>" + "<br>" + entry.getTitle() + "</br>" + "</h4>" + "</p>"
+                    + "<p>" + entry.getPublishedDate() + "</p>"
+                    + "<br>" + "<a href=" + entry.getLink() + ">" + "full article" + "</a>" + "</br>";
         }
 
-        XPath xpath = XPathFactory.newInstance().newXPath();
-        getNodeNameAndValue(doc, xpath);
-
-        return null;
-    }
-
-    private static void getNodeNameAndValue(Document doc, XPath xpath){
-        XPathExpression expr;
-        Object result = null;
-        try {
-            expr = xpath.compile("//channel/*/url/text()");
-            result = expr.evaluate(doc, XPathConstants.NODESET);
-        }
-        catch (XPathExpressionException e) {
-            e.printStackTrace();
-        }
-        NodeList nodes = (NodeList) result;
-
-        for (int i = 0; i < nodes.getLength(); i++) {
-            System.out.println(nodes.item(i).getParentNode().getNodeName() + " " + nodes.item(i).getNodeValue());
-        }
+        return parsedFeed;
     }
 }
