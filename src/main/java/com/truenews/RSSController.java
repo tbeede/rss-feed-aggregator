7bbd1009cdca112e836103eda64bfdfe982d8943
tbeede <trrbeede@gmail.com>
2018-02-18 13:01:48 -0500

    Add new RSS feeds

--- src/main/java/com/truenews/RSSController.java
+++ src/main/java/com/truenews/RSSController.java
@@ -3,19 +3,29 @@ package com.truenews;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.util.ArrayList;
+
+import static com.truenews.RSSParser.readRssFeed;
 
 @Controller
 public class RSSController {
     @GetMapping(value = "/")
-    public String rssForm(Model model) {
+    public String rssForm(Model model) throws Exception {
         Left left = new Left();
+        String readCNNFeed = readRssFeed(left.getCnn());
+        String readNYTFeed = readRssFeed(left.getNyt());
+        String readWAPOFeed = readRssFeed(left.getWaPo());
+        model.addAttribute("leftParserCNNFeed",readCNNFeed);
+        model.addAttribute("leftParserNYTFeed", readNYTFeed);
+        model.addAttribute("leftParserWAPOFeed", readWAPOFeed);
+
         Right right = new Right();
-        ArrayList getLeft = left.getLeftList();
-        ArrayList getRight = right.getRightList();
-        // RSSParser is WIP
-        model.addAttribute("leftParser", RSSParser.readRSSFeed());
-        model.addAttribute("rightParser", getRight);
+        String readFOXFeed = readRssFeed(right.getFox());
+        String readBLAZEFeed = readRssFeed(right.getBlaze());
+        String readNRFeed = readRssFeed(right.getNR());
+        model.addAttribute("rightParserFOXFeed", readFOXFeed);
+        model.addAttribute("rightParserBLAZEFeed", readBLAZEFeed);
+        model.addAttribute("rightParserNRFeed", readNRFeed);
+
         return "index";
     }
 }
